db5508bf281a3a7ff08e66539b84c1377f910e20
Robert Koeninger <rkoeninger@att.net>
2018-10-18 12:10:56 -0400

    Added distinct and flatten Array extensions

--- utils.js
+++ utils.js
@@ -10,6 +10,14 @@ Array.prototype.distinctBy = function (f) {
     });
 };
 
+Array.prototype.distinct = function () {
+    return _.uniq(this);
+};
+
+Array.prototype.flatten = function () {
+    return _.flatten(this);
+};
+
 String.prototype.contains = function (s) {
     return this.indexOf(s) >= 0;
 };
